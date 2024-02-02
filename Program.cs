7ba797f9eafd48d06ecd6da77bc8e482db1f4c35
AndreasCordeus <andreas_cordeus@hotmail.com>
2024-02-02 12:34:22 +0100

    update programcs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Use /crypt or /decrypt");
+app.MapGet("/", () => "Välkommen till Krypteraren! /n skriv /kryptera eller /avkryptera följt av ");
+
+app.MapGet("/kryptera", (string text, int skillnad) => Cipher(text, skillnad));
+
+app.MapGet("/avkryptera", (string text, int skillnad) => Cipher(text, -skillnad));
 
 app.Run();
+
+static string Cipher(string text, int skillnad)
+{
+    string resultat = "";
+
+    foreach (char c in text)
+    {
+        if (char.IsLetter(c))
+        {
+            char skillnadBokstav = (char)(c + skillnad);
+
+            if ((char.IsLower(c) && skillnadBokstav > 'z') || (char.IsUpper(c) && skillnadBokstav > 'Z'))
+            {
+                skillnadBokstav = (char)(c - (26 - skillnad));
+            }
+
+            resultat += skillnadBokstav;
+        }
+        else
+        {
+            resultat += c;
+        }
+    }
+
+    return resultat;
+}
