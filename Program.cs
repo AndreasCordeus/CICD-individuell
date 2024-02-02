c8a46af18e3460b903acb52a62b02968f4fce8bd
AndreasCordeus <andreas_cordeus@hotmail.com>
2024-02-02 09:51:22 +0100

    added endpoints

--- Program.cs
+++ Program.cs
@@ -3,4 +3,17 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Use /crypt or /decrypt");
 
+app.MapGet("/crypt", (int num1, int num2) => AddNumbers(num1, num2));
+
+app.MapGet("/decrypt", (int num1, int num2) => SubNumbers(num1, num2));
+
 app.Run();
+
+string AddNumbers(int num1, int num2)
+    {
+        return $"summan är {num1 + num2}";
+    }
+string SubNumbers(int num1, int num2)
+    {
+        return $"summan är {num1 - num2}";
+    }
