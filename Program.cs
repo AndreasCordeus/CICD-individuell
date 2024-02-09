330dc27868eb7849f2ab31cdc688561f04885244
AndreasCordeus <andreas_cordeus@hotmail.com>
2024-02-09 09:51:01 +0100

    update readme

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Use /crypt or /decrypt");
+app.MapGet("/", () => "Välkommen till Krypteraren! Skriv /kryptera?text= följt av ordet du vill kryptera och sedan &skillnad= följt av en siffra för hur många steg du vill hoppa i alfabetet, eller /avkryptera på samma sätt. Tex: text=Hej&skillnad=2");
+
+app.MapGet("/kryptera", (string text, int skillnad) => Cipher(text, skillnad));
+
+app.MapGet("/avkryptera", (string text, int skillnad) => Cipher(text, -skillnad));
 
 app.Run();
+
+static string Cipher(string text, int skillnad)
+{
+    string resultat = "";
+
+    foreach (char c in text)
+    {
+        if (char.IsLetter(c))
+        {
+            char skillnadBokstav = (char)(c + skillnad);
+
+            if ((char.IsLower(c) && skillnadBokstav > 'z') || (char.IsUpper(c) && skillnadBokstav > 'Z'))
+            {
+                skillnadBokstav = (char)(c - (26 - skillnad));
+            }
+
+            resultat += skillnadBokstav;
+        }
+        else
+        {
+            resultat += c;
+        }
+    }
+
+    return resultat;
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-Hejhopp
+Hej och välkommen till Krypteraren! 
+
+För att kryptera:
+    1. Följ länken http://cicd-individuell-env.eba-b2vz37zj.eu-north-1.elasticbeanstalk.com/kryptera?text=
+    2. Skriv sedan in texten du vill kryptera följt av &skillnad= (hur många bokstäver du vill hoppa fram i alfabetet).
+    3. Ett exempel på krypterad text: http://cicd-individuell-env.eba-b2vz37zj.eu-north-1.elasticbeanstalk.com/kryptera?text=Adde&skillnad=1
+       Detta visar texten Adde där alla bokstäver hoppat fram ett steg i alfabetet och visar då Beef.
+
+För att avkryptera:
+    1. Följ länken http://cicd-individuell-env.eba-b2vz37zj.eu-north-1.elasticbeanstalk.com/avkryptera?text=
+    2. Skriv sedan den krypterade texten följt av &skillnad= (hur många bokstäver i alfabetet den ska hoppa tillbaka).
+    3. Ett exempel på avkrypterad text: http://cicd-individuell-env.eba-b2vz37zj.eu-north-1.elasticbeanstalk.com/avkryptera?text=Beef&skillnad=1
+       Detta visar texten Beef där alla bokstäver hoppat bakåt ett steg i alfabetet och visar då Adde.
